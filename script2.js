6b45c22db1fd8866ad3c0ee295cf7f2854a41031
TonyMontano <anton.danilenko.06@mail.ru>
2026-03-03 09:42:38 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ let flag = true
 let str_nums = " " 
 while(flag)
 {
-let num = prompt("Введите число ", " ")
+let num = prompt("Введите число", " ")
 let num_num = parseInt(num)
 if(num_num%2==0)
 {
